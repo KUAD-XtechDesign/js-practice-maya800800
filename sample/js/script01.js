5ae214fba089964b5aa42399050bc604d69c1f46
maya800800 <m-wakabayashi0274@st.kyoto-art.ac.jp>
2021-10-08 15:42:55 +0900

    a

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,19 +1,35 @@
-$(function(){
+document.addEventListener('mousemove', function(e) {
+    let body = document.querySelector('body');
+    let circle = document.createElement('span');
+    let x = e.offsetX;
+    let y = e.offsetY;
+    circle.style.left = x + "px";
+    circle.style.top = y + "px";
+    let size = Math.random() * 100;
+    circle.style.width = 20 + size + "px";
+    circle.style.height = 20 + size + "px";
+    body.appendChild(circle);
+    setTimeout(function() {
+      circle.remove();
+    }, 1800);
+  });
+  
+  $(function(){
 
     $("#button01").on("click",function(){
      $(".back-red").hide();
     })
 
     $("#button02").on("click",function(){
-        $(".back-blue").hide();
+     $(".back-blue").hide();
     })
 
     $("#button03").on("click",function(){
-        $(".#box03,#box07,#box08").hide();
+     $("#box03,#box07,#box08").hide();
     })
 
     $("#button04").on("click",function(){
-        $(".box").show();
+     $(".box").show();
     })
 
 })
